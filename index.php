6f7dfd6eccee85632e57a51fa4d37f2076647611
eljack13 <97656656+Juanpro420@users.noreply.github.com>
2024-04-04 22:00:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EVIDENCIA DE APRENDIZAJE 2 </title>
+    <link rel="icon" type="image/jpeg" href="logo.jpeg">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="video-container">
+        <video width="100%" height="80%" autoplay muted>
+            <source src="video.mp4" type="video/mp4">
+        </video>
+        <div class="overlay">
+            <div class="logotipo">
+          <a href="index.php"> <img src="logo.jpeg" alt="" class="image" width="8%" border-radius="30px"></a> 
+            </div>
+            <nav>
+                <ul><a href="index.php">Inicio</a></ul>
+                <ul><a href="servicio.php">Servicios</a></ul>
+                <ul><a href="promociones.php">Promociones</a></ul>
+                <ul><a href="puntoventa.php">Punto de Venta</a></ul>
+            </nav>
+        </div>
+        <div class="contenido">
+            <p>El mejor servicio 🚗🇨🇼</p>
+            <button>
+  <a href="servicio.php" class="btn2"><span class="spn2">Servicios</span></a>
+</button>
+        </div>
+    </div>
+</body>
+
+</html>
