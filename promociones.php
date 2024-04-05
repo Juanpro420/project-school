1b8dab4027ddd7c3e8d17a8601b0f26167b8ff45
eljack13 <97656656+Juanpro420@users.noreply.github.com>
2024-04-04 22:07:23 -0600

    Add files via upload

---
+++ promociones.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylepromo.css">
+    <link rel="icon" type="image/jpeg" href="logo.jpeg">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="overlay">
+            <div class="logotipo">
+          <a href="index.php"> <img src="logo.jpeg" alt="" class="images" width="8%" border-radius="30px"></a> 
+            </div>
+            <nav>
+                <ul><a href="index.php">Inicio</a> </ul>
+                <ul><a href="servicio.php">Servicios</a></ul>
+                <ul><a href="promociones.php">Promociones</a></ul>
+                <ul><a href="puntoventa.php">Punto de Venta</a></ul>
+            </nav>
+        </div>
+</div>
+    </header>
+
+    <div class="contenido">
+    <h1>Promociones *Exclusivas* 
+    
+    </h1>
+    <p>Atendemos CDMX y Zona Metropolitana</p>
+    <div class="coooon">
+ <div class="paquetes">
+    <div class="card">
+        <div class="content">
+            <a href="#">
+                <span class="title">
+                    Promoción de Primavera
+                </span>
+            <p class="desc">
+                “Limpieza profunda de primavera - ¡Tu coche como nuevo! 2x1 en lavados completos durante todo el mes de abril.”
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="paquetes">
+    <div class="card">
+        <div class="content">
+            <a href="#">
+                <span class="title">
+                    Combo de Amigos
+                </span>
+            </a>
+            <p class="desc">
+                "Ven con un amigo y ambos recibirán un tratamiento de cera gratis al comprar un lavado completo. ¡Más amigos, más brillo!"
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="paquetes">
+    <div class="card">
+        <div class="content">
+            <a href="#">
+                <span class="title">
+                    Fidelidad Brillante
+                </span>
+            </a>
+            <p class="desc">
+                "Programa de lealtad - Acumula 5 lavados y obtén un encerado cerámico gratuito. ¡Para clientes que brillan!"
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="paquetes">
+    <div class="card">
+        <div class="content">
+            <a href="#">
+                <span class="title">
+                    Día de la Familia
+                </span>
+            </a>
+            <p class="desc">
+                "Sábados familiares - 20% de descuento en lavados para familias. ¡Los niños aprenden a cuidar su entorno mientras se divierten!"
+            </p>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<div class="bideo">
+    <video  width="500px" height="200px" autoplay muted>
+<source src="otro.mp4" type="video/mp4">
+</video>
+</div>
+</body>
+</html>
